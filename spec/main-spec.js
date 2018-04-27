63229d6d94c7fdac9e46698ab77fc22968124535
octopustail <494479914@qq.com>
2018-04-27 22:00:49 +0800

    测试

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,10 +1,14 @@
 let fs = require("fs");
-let main = require("src/index.js");
+let main = require("../src/index");
 
 describe('main()', () => {
 
     it('should pass', () => {
       expect(fs.statSync("src/index.html").isFile()).toBe(true);
     });
+    it('should return correct score',()=>{
+        let result = main();
+        expect(result).toEqual(0);
+    })
 
 });
